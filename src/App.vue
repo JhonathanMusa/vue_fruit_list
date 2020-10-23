fc857e4766db38ff61aad61503cd5e85e2871dc1
jhonathanmusa <jonathansm891@gmail.com>
2020-10-22 21:44:23 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <Cabecera />
+    <Lista />
+  </div>
+</template>
+
+<script>
+import Cabecera from "./components/Cabecera.vue";
+import Lista from "./components/Lista"
+
+export default {
+  name: "App",
+  components: {
+    Cabecera,
+    Lista
+  },
+};
+</script>
+
+
---
+++ src/components/Lista.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li
+        v-for="(item, index) of arrayOrdenado"
+        :key="item.id"
+        @click="aumentar(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      >
+        {{ index }} {{ item.nombre }}
+        <span class="badge badge-primary badge-pill"> {{ item.cantidad }}</span>
+      </li>
+      <button class="btn btn-danger btn-block" @click="reiniciar">Reiniciar</button>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "Lista",
+  computed: {
+    ...mapState(["frutas"]),
+    arrayOrdenado(){
+        // Ordena de menor a mayor
+        return this.frutas.sort((a, b)=> b.cantidad - a.cantidad )
+    }
+  },
+  methods: {
+    ...mapMutations(["aumentar", "reiniciar"]),
+  },
+};
+</script>
