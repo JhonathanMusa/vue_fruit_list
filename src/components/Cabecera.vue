03d7cd2a33a114afca39e5b4933dad6dfa7cdb52
jhonathanmusa <jhonathansamu91@gmail.com>
2020-12-31 22:22:41 -0500

    adding new features

--- src/components/Cabecera.vue
+++ src/components/Cabecera.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="text-center">
     <h1>Frut List</h1>
+    <input class="form-control" type="text" v-model="element" />
+    <p>{{ element }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Cabecera",
+  data() {
+    return {
+      element: "",
+    };
+  },
 };
 </script>
